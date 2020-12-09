b450259bff38f8ffb74e6006f6a81c7802ccea09
Kasim <kasim1411@gmail.com>
2020-12-09 22:41:37 +0300

    convert locale api url to release api url

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://instagram-mern-stack.herokuapp.com/',
 })
 
 export default API
--- front-end/src/components/Post/Post.js
+++ front-end/src/components/Post/Post.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
